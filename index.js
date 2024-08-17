fbacbfba7206a4ef29dbe37be413d042d741438b
deveshbis <deveshbiswas.cse@gmail.com>
2024-08-17 09:01:02 +0600

    Implement sorting functionality Price: Low to High, High to Low

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.95g0ypv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -79,7 +78,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send("Products server is running")
